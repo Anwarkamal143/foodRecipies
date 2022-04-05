2bbcdbd19d704ae226dbbc32e019fd2ffab1b33d
Umair <umairmanzoor1145@gmail.com>
2022-04-05 21:28:36 +0500

    Data show on click

--- src/components/ProfileTaskModel/dailyTask.tsx
+++ src/components/ProfileTaskModel/dailyTask.tsx
@@ -1,62 +1,171 @@
-import { EnvelopIcon, LampIcon, Ticke } from "@icons"
-import React from "react"
+import { EnvelopIcon, LampIcon, Loader, Ticke } from "@icons"
+import React, { useState } from "react"
 
-const days = ["1", "2", "3", "4", "5", "6", "7"]
-const Data = [
+
+const DataTwo = [
   {
-    status: "complete",
-    title: "Master Cook",
-    subtitle: "Upload more than 250 recipes",
+    day: 1,
+    data: [
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+    ],
   },
   {
-    status: "complete",
-    title: "Master Cook",
-    subtitle: "Upload more than 250 recipes",
+    day: 2,
+    data: [
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+    ],
   },
   {
-    status: "complete",
-    title: "Master Cook",
-    subtitle: "Upload more than 250 recipes",
+    day: 3,
+    data: [
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+    ],
+  },
+  {
+    day: 4,
+    data: [],
+  },
+  {
+    day: 5,
+    data: [
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+    ],
+  },
+  {
+    day: 6,
+    data: [
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+    ],
+  },
+  {
+    day: 7,
+    data: [
+      {
+        status: "complete",
+        title: "Master Cook",
+        subtitle: "Upload more than 250 recipes",
+      },
+    ],
   },
 ]
 type Props = {}
 
 function DailyTask({}: Props) {
+  const [taskData, setTaskData] = useState<any>([])
+  const DaliyTaskData = (item: any) => {
+    setTaskData(item)
+    return true
+  }
+  console.log(taskData, "DataTwo[item]")
   return (
     <div className="profileStatusModal">
       <div className="profileStatusWeekly">
-        {days.map(e => {
+        {DataTwo.map((e, ele) => {
           return (
             <>
-              <span>{e}</span>
+              <span
+                onClick={() => {
+                  DaliyTaskData(ele)
+                }}
+              >
+                {e.day}
+              </span>
             </>
           )
         })}
-        </div>
-        <div className="receipeDetailsWrap">
-          {(Data || [])?.map((u: any, i: number) => {
-            return (
-              <div className="receipeDetails" key={i}>
-                <div className="recipeholder">
-                  <span className="checkIcon">{u?.status === "complete" && <Ticke />}</span>
-                  <span className="imageIcon">
-                    <EnvelopIcon />
-                  </span>
-                  <div className="receipeTextbox">
-                    <span className="recipename">{u.title}</span>
-                    <div className="recipemeta">
-                      <div className="receipeRatings">{u.subtitle}</div>
-                    </div>
+      </div>
+      <div className="receipeDetailsWrap">
+        {(DataTwo[taskData]?.data || []).map((u: any, i: number) => {
+          return (
+            <div className="receipeDetails" key={i}>
+              <div className="recipeholder">
+                <span className="checkIcon">
+                  {u?.status === "complete" && <Ticke />}
+                </span>
+                <span className="imageIcon">
+                  <EnvelopIcon />
+                </span>
+                <div className="receipeTextbox">
+                  <span className="recipename">{u.title}</span>
+                  <div className="recipemeta">
+                    <div className="receipeRatings">{u.subtitle}</div>
                   </div>
+                  <Loader />
                 </div>
               </div>
-            )
-          })}
-        </div>
-        <div className="profileStatusModalButtons">
-          <a href="#" className="btnStart">Start</a>
-          <a href="#" className="btnIdeaLamp"><LampIcon /></a>
-        </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className="profileStatusModalButtons">
+        <a href="#" className="btnStart">
+          Start
+        </a>
+        <a href="#" className="btnIdeaLamp">
+          {/* <LampIcon /> */}
+        </a>
+      </div>
     </div>
   )
 }
