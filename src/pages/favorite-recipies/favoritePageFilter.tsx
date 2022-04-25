5509289f7c81d5c08213b91c466a212d13a0f73e
Abubakar Khaloid <uog.abubakar@gmail.com>
2022-04-25 21:37:01 +0500

    Filters Dropdown and Saved Recipes Page Header is Styled.

--- src/pages/favorite-recipies/favoritePageFilter.tsx
+++ src/pages/favorite-recipies/favoritePageFilter.tsx
@@ -10,46 +10,102 @@ function FavoriteFilerForm(props: IFilterFormProps) {
     const { className, onSubmit } = props
 
     return (
-        <div className={className}>
-            <span className="Cuisine">Cuisine</span>
-            <Checkbox width={16} height={16} label='American' />
-            <Checkbox width={16} height={16} label='Middle Eastern' />
-            <Checkbox width={16} height={16} label='Mediterranean' />
-            <span className="Meal-Type">Meal Type</span>
-            <Checkbox width={16} height={16} label='Side Dishes' />
-            <Checkbox width={16} height={16} label='Dinner' />
-            <Checkbox width={16} height={16} label='Lunch' />
-            <Checkbox width={16} height={16} label='Main Course' />
-            <Checkbox width={16} height={16} label='Deserts' />
-
-            <div className="option-type">
-                <Button shape="circle" size="small">
-                    Apply Filters
-                </Button>
-            </div>
+        <div className={`${className}`}>
+          <ul className="filtersDropdownList">
+            <li><span className="category-title">Cuisine</span></li>
+            <li><Checkbox width={16} height={16} label='American' /></li>
+            <li><Checkbox width={16} height={16} label='Middle Eastern' /></li>
+            <li><Checkbox width={16} height={16} label='Mediterranean' /></li>
+            <li><span className="category-title">Meal Type</span></li>
+            <li><Checkbox width={16} height={16} label='Side Dishes' /></li>
+            <li><Checkbox width={16} height={16} label='Dinner' /></li>
+            <li><Checkbox width={16} height={16} label='Lunch' /></li>
+            <li><Checkbox width={16} height={16} label='Main Course' /></li>
+            <li><Checkbox width={16} height={16} label='Deserts' /></li>
+          </ul>
+          <div className="filtersDropdownbutton">
+              <Button shape="circle" size="small">
+                  Apply Filters
+              </Button>
+          </div>
         </div>
     )
 }
 export default styled(FavoriteFilerForm)`
-  width: 20rem;
-  padding: 15px 20px 20px;
-  border-radius: 10px;
-  border: 1px solid #e5e8ef;
+  position: absolute;
+  width: 266px;
   background: #fff;
+  border: 1px solid #E5E8EF;
+  box-shadow: 0 10px 40px #E8E8FF;
+  border-radius: 15px;
+  margin-top: 5px;
+  padding: 10px 25px;
 
-  .option-type {
-    + .option-type {
-      padding-top: 12px;
+  .filtersDropdownList {
+    list-style: none;
+    padding: 0 0 15px;
+    margin: 0;
+    line-height: 0;
+
+    li {
+      padding: 10px 0;
+    }
+
+    .category-title {
+      display: block;
+      color: #1E1E2D;
+      font-size: 12px;
+      line-height: 20px;
+      padding-top: 5px;
+    }
+
+    .checkbox {
+      width: 100%;
+
+      svg {
+        left: auto;
+        right: 0;
+        z-index: 1;
+      }
     }
 
     label {
-      display: block;
-      font-size: 12px;
-      line-height: 20px;
-      color: #7b7b7b;
-      margin: 0 0 7px;
+      color: #7474A9;
+      font-size: 10px;
+      line-height: 14px;
+      cursor: pointer;
+      justify-content: space-between;
+
+      .label-data {
+        order: 1;
+        padding: 0 10px 0 0;
+      }
+
+      input {
+        order: 2;
+      }
     }
   }
 
+  .filtersDropdownbutton {
+    overflow: hidden;
+    text-align: center;
+    padding: 0 0 12px;
 
+    .button-default.button-sm {
+      width: 124px;
+      height: 40px;
+      color: #fff;
+      background:  #E0464D;
+      border-radius: 20px;
+      font-sze: 14px;
+      line-height: 18px;
+      padding: 7px 15px 13px;
+
+      &:hover {
+        background: #d02a31;
+        border-color: #d02a31;
+      }
+    }
+  }
 `
