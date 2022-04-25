52c56700aeb50b2765ab940a150327913acfa6d8
Umair <umairmanzoor1145@gmail.com>
2022-04-26 02:23:28 +0500

    fav pagination added

--- src/pages/favorite-recipies/index.tsx
+++ src/pages/favorite-recipies/index.tsx
@@ -1,24 +1,42 @@
-import { CircleDefaultImage, CircularProgresWithChildren, DropDown } from "@components"
+import {
+  CircleDefaultImage,
+  CircularProgresWithChildren,
+  DropDown,
+} from "@components"
 import { AngleRight, DownArrowIcon, FilterIcon, SearchIcon } from "@icons"
 import { PageLayoutWrapper } from "@layouts"
 import { FavData } from "data"
+import cloneDeep from "lodash/cloneDeep"
+import Pagination from "rc-pagination"
 import React from "react"
 import { LeftSide } from "src/components/common/UserProfile/userprofile.styled"
 import styled from "styled-components"
-import RecipesCard from '../../components/RecipiesCard'
+import RecipesCard from "../../components/RecipiesCard"
 import Button from "./../../components/Button/Button"
-import { Input } from './../../components/Input/Input'
+import { Input } from "./../../components/Input/Input"
 import { PopOver } from "./../../components/PopOver"
 import { FilterSection } from "./../../screens/MyFeeds/myfeeds.styled"
-import FavoriteFilerForm from './favoritePageFilter'
+import FavoriteFilerForm from "./favoritePageFilter"
+
 type Props = {
   className?: string
 }
 
 const Favorite = ({ className }: Props) => {
+  const countPerPage = 10
+  const [currentPage, setCurrentPage] = React.useState(1)
+  const [collection, setCollection] = React.useState(
+    cloneDeep(FavData.slice(0, countPerPage))
+  )
+  const updatePage = (p: any) => {
+    setCurrentPage(p)
+    const to = countPerPage * p
+    const from = to - countPerPage
+    setCollection(cloneDeep(FavData.slice(from, to)))
+  }
   return (
     <div className={`savedRecipesPage ${className}`}>
-      <PageLayoutWrapper className='savedRecipesWrapper' variant={'regular'}>
+      <PageLayoutWrapper className="savedRecipesWrapper" variant={"regular"}>
         <div className="recipesContainer">
           <header className="savedRecipesHeader">
             <LeftSide className="userProfileProgresBox">
@@ -41,16 +59,15 @@ const Favorite = ({ className }: Props) => {
                 <h2 className="userProfileTitle">
                   Welcome Back, <span>Omer Erdogan</span>
                 </h2>
-                <span className="userProfilesubTitle">Founder Cook Profile <AngleRight /></span>
+                <span className="userProfilesubTitle">
+                  Founder Cook Profile <AngleRight />
+                </span>
               </div>
             </LeftSide>
           </header>
           <FilterSection className="recipesFiltersForm">
             <div className="recipesFiltersbox">
-              <Input
-                type={'text'}
-                icon={<SearchIcon />}
-              />
+              <Input type={"text"} icon={<SearchIcon />} />
               <PopOver
                 button={
                   <Button
@@ -98,10 +115,19 @@ const Favorite = ({ className }: Props) => {
           </FilterSection>
           <div className="recipesSection">
             <div className="recipesSectionHolder">
-              <RecipesCard data={FavData} />
+              <RecipesCard data={collection} />
             </div>
           </div>
         </div>
+        <Pagination
+          pageSize={countPerPage}
+          onChange={updatePage}
+          current={currentPage}
+          total={FavData.length}
+          showPrevNextJumpers={true}
+          nextIcon="Next"
+          prevIcon="Previous"
+        />
       </PageLayoutWrapper>
     </div>
   )
