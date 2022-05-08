218c2a3d0a483b70795572fc7e1059e6130797de
Umair <umairmanzoor1145@gmail.com>
2022-05-09 01:15:09 +0500

    pancockPage pandding

--- src/screens/PanCake/index.tsx
+++ src/screens/PanCake/index.tsx
@@ -2,6 +2,7 @@ import { RecipesData } from "@redux/data"
 import { panCakeSliderData } from "data"
 import React, { useState } from "react"
 import SliderContent from "src/components/SliderContent"
+import Footer from "src/pages/NewPage/footer/footer"
 import LeftSide from "./components/LeftSide/index"
 import Nutrition from "./components/Nutrition"
 import Review from "./components/Review"
@@ -13,32 +14,35 @@ const PanCakeScreen = (props: Props) => {
   const [posts, setPosts] = useState([...RecipesData])
   const [singlePost, setSinglePost] = useState<any>({})
   return (
-    <div className="followersContainer">
-      {posts.splice(0, 1).map((post, i) => {
-        setSinglePost(post)
-        return (
-          <Post
-            key={post._id}
-            postFeed={post}
-            onSubmit={post => {
-              const newPosts = [...posts]
-              const index = newPosts.findIndex(p => p._id === post._id)
-              if (index !== -1) {
-                newPosts[index] = post
-                setPosts(posts => newPosts)
-              }
-            }}
-          />
-        )
-      })}
-      <LeftSide post={singlePost} />
-      <RightSide />
-      <Nutrition />
-      <Review />
-      {/* <div className="recipesSection"> */}
-      <h2>Recommended Recipes</h2>
-      <SliderContent data={panCakeSliderData} />
-      {/* </div> */}
+    <div className="ctn">
+      <div className="followersContainer">
+        {posts.splice(0, 1).map((post, i) => {
+          setSinglePost(post)
+          return (
+            <Post
+              key={post._id}
+              postFeed={post}
+              onSubmit={post => {
+                const newPosts = [...posts]
+                const index = newPosts.findIndex(p => p._id === post._id)
+                if (index !== -1) {
+                  newPosts[index] = post
+                  setPosts(posts => newPosts)
+                }
+              }}
+            />
+          )
+        })}
+        <LeftSide post={singlePost} />
+        <RightSide />
+        <Nutrition />
+        <Review />
+        {/* <div className="recipesSection"> */}
+        <h2>Recommended Recipes</h2>
+        <SliderContent data={panCakeSliderData} />
+        {/* </div> */}
+      </div>
+      <Footer />
     </div>
   )
 }
