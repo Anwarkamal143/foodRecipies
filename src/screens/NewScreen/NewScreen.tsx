de21a02bcf5c55a87be4a3832e262d934cef71b7
Abubakar Khalid <uog.abubakar@gmail.com>
2022-05-24 16:12:41 +0500

    done with newpage1

--- src/screens/NewScreen/NewScreen.tsx
+++ src/screens/NewScreen/NewScreen.tsx
@@ -37,49 +37,114 @@ const NewScreen = ({ className }: Props) => {
   }
   return (
     <div className={classNames(className)}>
-      <Banner
-        title="All Online Cooks"
-        subTitle="Explore recipes from across the world. Looking to make some homemade pasta? We have plenty of Italian recipes! Looking to challenge yourself and cook something new from another country? Check out a new cuisine below."
-        lottieAnimation={
-          <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
-        }
-      />
-      <SearchBar />
-      <div>
-        {[10, 2].map((c, i) => (
-          <SocialCard
-            key={i}
-            type="primary"
-            profileImage="/images/profile-img.png"
-            bgImage="/images/mock/post1.png"
-            title="Jessica Jones"
-            date="05/28/22"
-            icon={{
-              facebook: <FacebookIcon />,
-              youtube: <YoutubeIcon />,
-              twitter: <TwitterIcon />,
-              instagram: <InstagramIcon />,
-            }}
-            footerValue={{
-              views: "2.5k",
-              recipies: "35",
-              saves: "230",
-            }}
+      <div className="container px-4 mx-auto sm:px-6 lg:px-8 pb-80">
+        <Banner
+          title="All Online Cooks"
+          subTitle="Explore recipes from across the world. Looking to make some homemade pasta? We have plenty of Italian recipes! Looking to challenge yourself and cook something new from another country? Check out a new cuisine below."
+          lottieAnimation={
+            <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
+          }
+        />
+        <SearchBar />
+        <div className="cards-frame">
+          <div className="cards-row">
+            {[10, 2].map((c, i) => (
+              <SocialCard
+                key={i}
+                type="primary"
+                profileImage="/images/profile-img.png"
+                bgImage="/images/img-cook.jpg"
+                title="Jessica Jones"
+                date="05/28/22"
+                icon={{
+                  facebook: <FacebookIcon />,
+                  youtube: <YoutubeIcon />,
+                  twitter: <TwitterIcon />,
+                  instagram: <InstagramIcon />,
+                }}
+                footerValue={{
+                  views: "2.5k",
+                  recipies: "35",
+                  saves: "230",
+                }}
+              />
+            ))}
+          </div>
+          <Pagination
+            pageSize={countPerPage}
+            onChange={updatePage}
+            current={currentPage}
+            total={RecipesData.length}
+            showPrevNextJumpers={true}
+            nextIcon="Next"
+            prevIcon="Previous"
           />
-        ))}
+        </div>
       </div>
-      <Pagination
-        pageSize={countPerPage}
-        onChange={updatePage}
-        current={currentPage}
-        total={RecipesData.length}
-        showPrevNextJumpers={true}
-        nextIcon="Next"
-        prevIcon="Previous"
-      />
       <Footer />
     </div>
   )
 }
 
-export const MyNewScreen = styled(NewScreen)``
+export const MyNewScreen = styled(NewScreen)`
+  .pb-80 {
+    padding-bottom: 80px;
+  }
+
+  .pageFiltersSearch {
+    padding-left: 4rem;
+    padding-right: 4rem;
+
+    @media (max-width: 1279px) {
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+
+    @media (max-width: 767px) {
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+  }
+
+  .cards-frame {
+    padding: 0 8.3rem;
+
+    @media (max-width: 1279px) {
+      padding: 0 40px;
+    }
+
+    @media (max-width: 767px) {
+      padding: 0 10px;
+    }
+  }
+
+  .cards-row {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -12px;
+
+    @media (max-width: 767px) {
+      margin: 0 -9px;
+    }
+
+    .card-primary {
+      padding: 0 12px;
+      margin: 0 0 38px;
+      width: 25%;
+
+      @media (max-width: 1023px) {
+        width: 33.333%;
+      }
+
+      @media (max-width: 767px) {
+        width: 50%;
+        margin: 0 0 25px;
+        padding: 0 9px;
+      }
+    }
+  }
+
+  .rc-pagination {
+    padding-top: 0;
+  }
+`
